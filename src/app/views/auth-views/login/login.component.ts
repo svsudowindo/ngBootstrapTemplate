dffcf6a69f0ae3941d2ebb0e2b6bb680fd670ac7
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-05-22 17:39:17 +0530

    EXCEL DOWNLOAD

--- src/app/views/auth-views/login/login.component.ts
+++ src/app/views/auth-views/login/login.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit, Injector } from '@angular/core';
+import { Component, OnInit, Injector, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { BaseClass } from './../../../shared/services/common/baseClass';
 import { RequestEnums } from '../../../shared/constants/request-enums';
-import { GlobalVariables } from '../../../shared/services/common/globalVariables';
-import { GlobalVariableEnums } from '../../../shared/constants/gloabal-variable-enums';
 import Utils from './../../../shared/services/common/utils';
 import { CommonRequestService } from '../../../shared/services/common-request.service';
 import { PopupService } from '../../../shared/components/componentsAsService/popup/popup.service';
 import { POPUP } from '../../../shared/constants/popup-enum';
 import { IDataInfo } from '../../../shared/components/componentsAsService/popup/popup-info.service';
 import { LoaderService } from '../../../shared/components/componentsAsService/loader/loader.service';
+import { GlobalVariables } from '../../../shared/services/common/globalVariables';
+import { GlobalVariableEnums } from '../../../shared/constants/gloabal-variable-enums';
+import 'jspdf-autotable';
+import * as jspdf from 'jspdf';
+declare var jsPDF: any;
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-login',
@@ -18,6 +22,26 @@ import { LoaderService } from '../../../shared/components/componentsAsService/lo
 })
 export class LoginComponent extends BaseClass implements OnInit {
 
+  info = [
+    {
+      name: 'sai',
+      number: 81251
+    },
+    {
+      name: 'sample',
+      number: 81252
+    },
+    {
+      name: 'sample3',
+      number: 81253
+    },
+    {
+      name: 'sample4',
+      number: 81254
+    },
+  ];
+
+  @ViewChild('contentToConvert') contentToConvert: ElementRef;
   public successMessageStatus: string;
   public errorMessageStatus: string;
 
@@ -67,4 +91,31 @@ export class LoginComponent extends BaseClass implements OnInit {
   openLoader() {
     this._loaderService.showLoading();
   }
+
+  downloadTableAsPdf() {
+    const doc = new jsPDF();
+    doc.autoTable({ html: '#contentToConvert' });
+    doc.save('table.pdf');
+  }
+  downloadPdfAsImage() {
+    // image download
+    const data = this.contentToConvert.nativeElement;
+    // const clientWidth = data.clientWidth;
+    html2canvas(data).then(canvas => {
+      // Few necessary setting options
+      console.log(canvas);
+      const imgWidth = 200;
+      const pageHeight = 100;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('MYPdf.pdf'); // Generated PDF
+    });
+  }
+
+
 }
